af44c444b88876e8af9a7202de756e3d67d3cc1e
Alejandro Santiago <alemures@hotmail.com>
2021-05-21 13:00:02 +0200

    Added pathSeparator option to README
    Also improved examples

--- example/basic.js
+++ example/basic.js
@@ -7,28 +7,21 @@ const data = JSON.stringify({
   spain: {
     people: [{ name: 'Antonio' }, { name: 'Juan' }, { name: 'Pedro' }],
   },
-  'unknown.country': {
-    people: [{ name: 'Frank' }, { name: 'Paul' }],
-  },
 });
 
 const fastJson = new FastJson();
 
+// Path is a string representing a javascript object path
 fastJson.on('ireland.people', (value) => {
   console.log('ireland.people ->', value);
 });
 
-fastJson.on('spain.people[1].name', (value) => {
-  console.log('spain.people[1].name ->', value);
+// Paths can be also an array of keys
+fastJson.on(['spain', 'people', '1', 'name'], (value) => {
+  console.log(['spain', 'people', '1', 'name'], '->', value);
 });
 
-// Path as Array to allow keys with dots
-fastJson.on(['unknown.country', 'people', '0', 'name'], (value) => {
-  console.log(['unknown.country', 'people', '0', 'name'], value);
-  // Stop parsing JSON usefull when have all we need improving performance
-  fastJson.skip();
-});
-
+// Wildcards can be used to match all items in object or array
 fastJson.on('spain.people[*].name', (value) => {
   console.log('spain.people[*].name ->', value);
 });
@@ -38,3 +31,5 @@ fastJson.on('*.people[*].name', (value) => {
 });
 
 fastJson.write(data);
+// The JSON can be string or Buffer
+// fastJson.write(Buffer.from(data))
